f50dbf1ca786e63cc702c79cc61db95419528444
teigorme <igorteixeira354@gmail.com>
2025-06-12 18:35:30 +0100

    feat: add middleware for 404 and 500

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
 import helmet from "helmet";
 import cors from "cors";
 import morgan from "morgan";
@@ -10,7 +10,26 @@ app.use(helmet());
 app.use(cors());
 app.use(morgan("dev"));
 
-app.get("/", (request, response) => {
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(404).json({
+    status: 404,
+    message: "Route not found.",
+    path: req.originalUrl,
+  });
+});
+
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  res.status(500).json({
+    status: 500,
+    message: "Internal server error.",
+    error:
+      process.env.NODE_ENV === "development"
+        ? err.message
+        : "Something went wrong.",
+  });
+});
+
+app.get("/", (_, response) => {
   response.status(200).json({ message: "Good Luck" });
 });
 
